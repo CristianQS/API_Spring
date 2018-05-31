99db1630da749b8fbd82f9471a18036ae59bb6f3
FranMontielGonzalez <fran_mont_gonz@hotmail.com>
2018-05-31 13:07:05 +0100

    first commit

---
+++ src/main/java/ApiDAW2/Application.java
@@ -0,0 +1,13 @@
+package ApiDAW2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
